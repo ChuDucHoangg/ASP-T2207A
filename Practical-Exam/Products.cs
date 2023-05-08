eab7fa1470aa6cf68b29fe46d002e0b687cb2cd3
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2023-05-08 15:11:20 +0700

    Practical-Exam-C#

---
+++ Practical-Exam/Products.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace Practical_Exam
+{
+	public class Products
+	{
+		private string idProduct { get; set; }
+		private string nameProduct { get; set; }
+		private int priceProduct { get; set; }
+        private static List<Products> productList = new List<Products>();
+
+        public Products(string idProduct, string nameProduct, int priceProduct)
+		{
+			this.idProduct = idProduct;
+			this.nameProduct = nameProduct;
+			this.priceProduct = priceProduct;
+		}
+
+		public Products()
+		{
+		}
+
+		public void addProduct()
+		{
+			Console.WriteLine("Enter product ID: ");
+			string enterID = Console.ReadLine();
+			this.idProduct = enterID;
+
+			Console.WriteLine("Enter product name: ");
+			string enterName = Console.ReadLine();
+			this.nameProduct = enterName;
+
+			Console.WriteLine("Enter product price: ");
+			string enterPrice = Console.ReadLine();
+			priceProduct = Convert.ToInt32(enterPrice);
+
+            productList.Add(this);
+        }
+
+		public void displayProduct()
+		{
+            Console.WriteLine("Product ID\tProductName\tPrice");
+			foreach (var product in productList)
+			{
+				Console.WriteLine($"{product.idProduct}\t\t{product.nameProduct}\t\t${product.priceProduct}");
+			}
+        }
+
+		public void deleteProduct()
+		{
+			Console.WriteLine("Enter product ID to delete: ");
+			string deleteID = Console.ReadLine();
+
+			foreach (var product in productList)
+			{
+				if (deleteID == idProduct)
+				{
+					productList.Remove(product);
+					Console.WriteLine("Product deleted!");
+					return;
+				}
+				else
+				{
+					Console.WriteLine("Product not found!");
+				}
+			}
+		}
+    }
+}
+
---
+++ Practical-Exam/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Practical_Exam;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Products p = new Products();
+
+        while (true)
+        {
+            Console.WriteLine("Choose method: ");
+            Console.WriteLine("1. Add product");
+            Console.WriteLine("2. Display product");
+            Console.WriteLine("3. Delete product");
+            Console.WriteLine("4. Exit");
+
+            Console.WriteLine("Enter your choose: ");
+            string choose = Console.ReadLine();
+
+            switch (choose)
+            {
+                case "1":
+                    p.addProduct();
+                    break;
+
+                case "2":
+                    p.displayProduct();
+                    break;
+
+                case "3":
+                    p.deleteProduct();
+                    break;
+                case "4":
+                    return;
+            }
+        }
+    }
+}
+
